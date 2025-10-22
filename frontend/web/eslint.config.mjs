c42f5ccb14228527d658bb4e8a5bab4659b820ba
Jackson Davis <jwd0526@gmail.com>
2025-10-22 13:57:55 -0400

    Create seperate jobs for web and native linting

--- frontend/web/eslint.config.mjs
+++ frontend/web/eslint.config.mjs
@@ -1,19 +1,14 @@
-import { fileURLToPath } from 'node:url';
-import { dirname } from 'node:path';
-import { FlatCompat } from '@eslint/eslintrc';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends('next/core-web-vitals'),
-  {
-    ignores: ['.next/**', 'out/**', 'build/**', 'next-env.d.ts'],
-  },
-];
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  globalIgnores([
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+  ]),
+]);
 
 export default eslintConfig;
