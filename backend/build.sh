569b88cb4a6235dde70fbe210a1bcedabf621e5f
Jackson Davis <jwd0526@gmail.com>
2025-10-23 14:16:46 -0400

    Compile and generate sqlc code before build

--- backend/build.sh
+++ backend/build.sh
@@ -2,7 +2,14 @@
 
 set -e
 
-echo "Building Go backend..."
+echo "Generating sqlc code..."
+
+sqlc compile
+
+sqlc generate
+
+echo "Building backend..."
+
 go build -o bin/brewd-backend ./cmd/server
 
 echo "Backend build complete!"
